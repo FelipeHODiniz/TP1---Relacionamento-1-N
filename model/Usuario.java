d5febc79590492b137f0ea2b91af3b3733575323
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2026-03-20 15:06:45 -0300

    feat: setHashsenha

--- model/Usuario.java
+++ model/Usuario.java
@@ -46,6 +46,10 @@ public class Usuario implements Registro {
         return email;
     }
 
+    public void setHashSenha(final String hashSenha){
+        this.hashSenha = hashSenha;
+    }
+
     public String getHashSenha() {
         return hashSenha;
     }
